cf6a408d65e11ab0f30a3a58b4e0e3a0599243da
sametsafkan <D6cf53ec36>
2020-04-12 17:16:46 +0300

    initial commit.

---
+++ src/main/java/com/sametsafkan/mssbreweryclient/MssBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssBreweryClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sametsafkan/mssbreweryclient/MssBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametsafkan.mssbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssBreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
